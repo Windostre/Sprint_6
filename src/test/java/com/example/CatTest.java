b354bc8cdc3bb63a14f7b6ef7a13bd50dc40b083
Windostre <windostre@gmail.com>
2022-11-24 23:39:54 +0300

    Созданы классы тестов для классов Cat, Lion, Feline

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+public class CatTest {
+
+    @org.junit.Test
+    public void getSound() {
+    }
+
+    @org.junit.Test
+    public void getFood() {
+    }
+}
