b8729b1240c41ce58dc10205b1adbccfd974153e
Windostre <windostre@gmail.com>
2022-11-25 18:57:00 +0300

    Добавлен тест для getKittens() класс Lion

---
+++ src/test/java/com/example/lionTests/LionTestGetKitten.java
@@ -0,0 +1,27 @@
+package com.example.lionTests;
+
+import com.example.Lion;
+import com.example.Predator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTestGetKitten {
+
+    @Mock
+    Predator predator;
+
+    @Test
+    public void getKittensArgument0lReturns0() throws Exception {
+        Lion lion = new Lion("Самец", predator);
+        Mockito.when(predator.getKittens()).thenReturn(1);
+        assertEquals(1, lion.getKittens());
+
+    }
+
+}
