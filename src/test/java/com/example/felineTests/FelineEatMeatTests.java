d9940fd06a92b79faab3a0555ca489c4fd47b77b
Windostre <windostre@gmail.com>
2022-11-25 12:43:56 +0300

    оптимизирован код теста для FelineEatMeatTests

--- src/test/java/com/example/felineTests/FelineEatMeatTests.java
+++ src/test/java/com/example/felineTests/FelineEatMeatTests.java
@@ -2,18 +2,21 @@ package com.example.felineTests;
 
 import com.example.Feline;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class FelineEatMeatTests {
     Feline feline = new Feline();
+
     @Test
-    public void eatMeatReturnPredatorFood() throws Exception {
+    public void eatMeatReturnFelineFood() {
         List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
-        assertEquals(expectedList, feline.eatMeat());
+        List<String> actualList = null;
+        try {
+            actualList = feline.eatMeat();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(expectedList, actualList);
     }
 }
