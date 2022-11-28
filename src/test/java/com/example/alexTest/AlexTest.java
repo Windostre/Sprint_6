19887e0770421f9af2b8b202e4aa0d78dc00f18b
Windostre <windostre@gmail.com>
2022-11-28 21:57:14 +0300

    Созданы юнит тесты на класс Alex

---
+++ src/test/java/com/example/alexTest/AlexTest.java
@@ -0,0 +1,44 @@
+package com.example.alexTest;
+
+import com.example.Alex;
+import com.example.Feline;
+import com.example.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AlexTest {
+
+    @Mock
+    Feline feline;
+
+
+
+    @Test
+    public void getPlaceOfLiving() throws Exception {
+        Alex alex = new Alex(feline);
+        String expectedText = "Нью-Йоркский зоопарк";
+        assertEquals(expectedText, alex.getPlaceOfLiving());
+
+    }
+
+    @Test
+    public void friendsNames() throws Exception {
+        Alex alex = new Alex(feline);
+        List<String> expectedList = List.of("Марти", "Глория", "Мелман");
+        assertEquals(expectedList, alex.friendsNames());
+    }
+
+    @Test
+    public void getKittens() throws Exception {
+        Alex alex = new Alex(feline);
+        assertEquals(0, alex.getKittens());
+    }
+}
